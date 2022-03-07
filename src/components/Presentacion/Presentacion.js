45f2a2f437d674d9a86d8e9bb9ae5ef7618c3554
CuerpoCasa <gabialberini733@gmail.com>
2022-03-07 20:14:47 -0300

    V2.1

--- src/components/Presentacion/Presentacion.js
+++ src/components/Presentacion/Presentacion.js
@@ -7,7 +7,7 @@ const Presentacion = () => {
                 <img className='img' src="https://i.ibb.co/PNKxZRN/pixlr-bg-result.png" alt="imagen de presentacion" />
             </div>
             <div className='cont-sombra'></div>
-            <h1>¡Próximamente!</h1>
+            <h1 className='title'>¡Próximamente!</h1>
         </div>
     )
 }
