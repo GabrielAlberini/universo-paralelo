99ccaea7fc791c7016527b5f12ae7400e42533b0
CuerpoCasa <gabialberini733@gmail.com>
2022-03-16 09:32:51 -0300

    V2.9

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,52 @@
-import { ContainerFooter, ItemFooter, LinkItemFooter, TxtItemFooter, IconFooter, ContainerIconFooter} from '../../StyledComponents/Footer'
+import {
+  ContainerFooter,
+  ItemFooter,
+  LinkItemFooter,
+  TxtItemFooter,
+  IconFooter,
+  ContainerIconFooter,
+} from "../../StyledComponents/Footer";
 // import { Link } from 'react-router-dom'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-    return (
-      <ContainerFooter>
-        <ItemFooter>
-          <TxtItemFooter>Seguinos en Instagram!</TxtItemFooter>
-          <ContainerIconFooter>
-            <IconFooter icon={faInstagram} /> 
-          </ContainerIconFooter>
-        </ItemFooter> 
-        <ItemFooter>
-          <TxtItemFooter>Universo Paralelo</TxtItemFooter>
-        </ItemFooter>
-        <ItemFooter>
-          <TxtItemFooter>Powered by <LinkItemFooter href="https://gabrielalberini.vercel.app" target="_blank" rel='noreferrer'>Gabriel Alberini</LinkItemFooter></TxtItemFooter>
-        </ItemFooter>
-      </ContainerFooter>
-    );
-  }
-  
-  export {Footer};
+  return (
+    <ContainerFooter>
+      <ItemFooter>
+        <TxtItemFooter>Seguinos en Instagram!</TxtItemFooter>
+        <ContainerIconFooter>
+          <a
+            href="https://www.instagram.com/radio.universoparalelo/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <IconFooter icon={faInstagram} />
+          </a>
+        </ContainerIconFooter>
+      </ItemFooter>
+      <ItemFooter>
+        <div style={{ width: 150 }}>
+          <img
+            style={{ width: "100%" }}
+            src="https://i.ibb.co/PNKxZRN/pixlr-bg-result.png"
+            alt="logo de programa"
+          />
+        </div>
+      </ItemFooter>
+      <ItemFooter>
+        <TxtItemFooter>
+          Powered by{" "}
+          <LinkItemFooter
+            href="https://www.linkedin.com/in/gabriel-alberini/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Gabriel Alberini
+          </LinkItemFooter>
+        </TxtItemFooter>
+      </ItemFooter>
+    </ContainerFooter>
+  );
+}
+
+export { Footer };
