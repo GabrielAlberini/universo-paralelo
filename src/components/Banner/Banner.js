7f7e047e5e9fa6fd81ab5e7ebecd74e94655817a
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2022-12-26 13:10:43 -0300

    Modificacion foto de banner por navideña

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,11 @@
 import { ContainerBanner, ImgBanner } from "../../StyledComponents/Banner";
 
 const Banner = () => {
-    return (
-      <ContainerBanner>
-        <ImgBanner src="https://i.ibb.co/G00nmkg/PORTADA-WEB-NUEVA-Mesa-de-trabajo-1-1.jpg"/>
-      </ContainerBanner>
-    );
-  }
+  return (
+    <ContainerBanner>
+      <ImgBanner src="https://i.ibb.co/rM6N7y9/banner-navidad.jpg" />
+    </ContainerBanner>
+  );
+};
 
-  export {Banner}; 
+export { Banner };
