bc7e2347fef0c7cfd5aadeeb16dcd39d355a0490
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2022-12-26 13:24:50 -0300

    Modificación de imagen navideña en header, remidencionada

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import { ContainerBanner, ImgBanner } from "../../StyledComponents/Banner";
 const Banner = () => {
   return (
     <ContainerBanner>
-      <ImgBanner src="https://i.ibb.co/rM6N7y9/banner-navidad.jpg" />
+      <ImgBanner src="https://i.ibb.co/qYH61D5/NUEVO-PARA-SITIO-WEB2.jpg" />
     </ContainerBanner>
   );
 };
