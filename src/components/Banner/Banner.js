420b9effd04fa7db604d5e641ca04593531de9aa
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-04-03 20:38:36 -0300

    Modificación de foto de banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import { ContainerBanner, ImgBanner } from "../../StyledComponents/Banner";
 const Banner = () => {
   return (
     <ContainerBanner>
-      <ImgBanner src="https://i.ibb.co/qYH61D5/NUEVO-PARA-SITIO-WEB2.jpg" />
+      <ImgBanner src="https://i.ibb.co/pZ7TH4Q/1-con-tomate-nueva.jpg" />
     </ContainerBanner>
   );
 };
