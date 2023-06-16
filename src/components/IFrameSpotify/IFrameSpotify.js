29f7d424669ebda3421ce4e2d0fdc1d183698dcd
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2023-06-16 13:27:40 -0300

    Fix eframe spotify

--- src/components/IFrameSpotify/IFrameSpotify.js
+++ src/components/IFrameSpotify/IFrameSpotify.js
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 
 const IFrameSpotify = () => {
-    return <ActicleIFrame dangerouslySetInnerHTML={{ __html: '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/show/3iOWz7ZfFdVdZNgXWoO8ls?utm_source=generator" width="100%" height="160" frameBorder="0" allowfullscreen=""></iframe>'}} />;
-}
+  return (
+    <ActicleIFrame
+      dangerouslySetInnerHTML={{
+        __html:
+          '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/show/3iOWz7ZfFdVdZNgXWoO8ls?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen=""></iframe>',
+      }}
+    />
+  );
+};
 
 const ActicleIFrame = styled.article`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-export { IFrameSpotify }
-
+export { IFrameSpotify };
