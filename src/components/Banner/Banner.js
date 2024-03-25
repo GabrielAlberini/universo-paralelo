e22df39b3ee4e6f603189d85dbd3c6a72edaf277
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-03-25 11:30:22 -0300

    Banner imagen actualizada

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import { ContainerBanner, ImgBanner } from "../../StyledComponents/Banner";
 const Banner = () => {
   return (
     <ContainerBanner>
-      <ImgBanner src="https://i.ibb.co/pZ7TH4Q/1-con-tomate-nueva.jpg" />
+      <ImgBanner src="https://i.ibb.co/7t8xRwc/FINAL-NUEVO-SITIO-WEB.jpg" />
     </ContainerBanner>
   );
 };
